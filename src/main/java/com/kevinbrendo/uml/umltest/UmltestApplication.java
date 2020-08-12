8c72062f8f4a626cc965c778825f0be6b9ed95b7
kevinbrendo <kevinbrendo@outlook.com>
2020-08-12 15:39:32 -0300

    projeto criado

---
+++ src/main/java/com/kevinbrendo/uml/umltest/UmltestApplication.java
@@ -0,0 +1,15 @@
+package com.kevinbrendo.uml.umltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmltestApplication {
+
+	public static void main(String[] args) {
+		
+
+		SpringApplication.run(UmltestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinbrendo/uml/umltest/UmltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinbrendo.uml.umltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
