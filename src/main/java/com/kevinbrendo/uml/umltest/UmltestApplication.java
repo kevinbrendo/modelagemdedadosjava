c8ab5b9cd1442ce1a64e384c333f05e5c3fbfd34
kevinbrendo <kevinbrendo@outlook.com>
2020-08-12 15:49:03 -0300

    test rest

--- src/main/java/com/kevinbrendo/uml/umltest/UmltestApplication.java
+++ src/main/java/com/kevinbrendo/uml/umltest/UmltestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UmltestApplication {
 
 	public static void main(String[] args) {
-		
+
 
 		SpringApplication.run(UmltestApplication.class, args);
 	}
