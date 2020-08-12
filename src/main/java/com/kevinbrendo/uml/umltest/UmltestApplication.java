a5f4c9fe1376e74039f4dad468f38d6d6ccb1440
kevinbrendo <kevinbrendo@outlook.com>
2020-08-12 16:06:29 -0300

    adicionado classe categoria

--- src/main/java/com/kevinbrendo/uml/umltest/UmltestApplication.java
+++ src/main/java/com/kevinbrendo/uml/umltest/UmltestApplication.java
@@ -3,11 +3,11 @@ package com.kevinbrendo.uml.umltest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class UmltestApplication {
 
 	public static void main(String[] args) {
-
 
 		SpringApplication.run(UmltestApplication.class, args);
 	}
